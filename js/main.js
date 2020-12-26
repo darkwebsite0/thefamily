87f18c46dbcbe139b061d26385bf3b8d82699004
darkwebsite0 <76429009+darkwebsite0@users.noreply.github.com>
2020-12-26 13:05:30 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const menuBtn = document.querySelector(".menu-icon span");
+    const searchBtn = document.querySelector(".search-icon");
+    const cancelBtn = document.querySelector(".cancel-icon");
+    const items = document.querySelector(".nav-items");
+    const form = document.querySelector("form");
+    const img = document.querySelector("img");
+    const icons = document.querySelector(".icons");
+    const profileimg = document.querySelector(".profile-img"); 
+
+    menuBtn.onclick = ()=>{
+      items.classList.add("active");
+      menuBtn.classList.add("hide");
+      searchBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+      profileimg.classList.add("hide");
+    }
+    cancelBtn.onclick = ()=>{
+      items.classList.remove("active");
+      menuBtn.classList.remove("hide");
+      searchBtn.classList.remove("hide");
+      cancelBtn.classList.remove("show");
+      form.classList.remove("active");
+      cancelBtn.style.color = "#ff3d00";
+    }
+    searchBtn.onclick = ()=>{
+      form.classList.add("active");
+      searchBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+    }
+
+    img.onclick = function(){
+        this.classList.toggle("active");
+        icons.classList.toggle("active");
+    }
+
+    
